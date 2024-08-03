352e7c59f6af2a0108af3db6c85a475e903ab370
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-08-03 16:41:01 +0200

    changed popup component to see if it works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,46 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
+import { Component } from '@angular/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatDialogModule } from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
-@NgModule({
+@Component({
+  selector: 'app-add-project-popup',
+  templateUrl: './add-project-popup.component.html',
+  styleUrls: ['./add-project-popup.component.css'],
+  standalone: true,
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    FormsModule,
+    CommonModule,
+    MatDialogModule,
+    MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
     MatButtonModule,
     MatButtonToggleModule,
-    MatDialogModule
-  ],
-  providers: []
+    FormsModule
+  ]
 })
-export class AppModule { }
+export class AddProjectPopupComponent {
+  projectCatalogNumber: string = '';
+  artist: string = '';
+  projectTitle: string = '';
+  label: string = '';
+  albumType: string = '';
+  labels: string[] = ['Label1', 'Label2', 'Label3', 'Label4', 'Label5'];
+
+  onCancel(): void {
+    // Logic for cancel button
+  }
+
+  onAddProject(): void {
+    // Logic for add project button
+  }
+
+  formValid(): boolean {
+    return !!this.projectCatalogNumber && !!this.artist && !!this.projectTitle && !!this.label && !!this.albumType;
+  }
+}
