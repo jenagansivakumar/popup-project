0b8866037ddd077e8a457d02f16f960ab4d4d443
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-08-03 15:25:00 +0200

    imported components and modules

---
+++ src/app/app.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { AppComponent } from "./app.component";
+import { MainComponent } from "./main/main.component";
+import { AddProjectPopupComponent } from "./add-project-popup/add-project-popup.component";
