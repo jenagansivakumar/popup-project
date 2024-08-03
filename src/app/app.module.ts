429b43e18794d12e31fd4068a7c20a3e4990e5b0
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-08-03 15:36:27 +0200

    imported angular material modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,3 +5,9 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppComponent } from "./app.component";
 import { MainComponent } from "./main/main.component";
 import { AddProjectPopupComponent } from "./add-project-popup/add-project-popup.component";
+
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatDialogModule } from '@angular/material/dialog';
