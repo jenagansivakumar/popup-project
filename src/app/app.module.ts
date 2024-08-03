0fd2f65e7903e495482d20498641329361620e66
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-08-03 16:02:57 +0200

    added imports to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { MainComponent } from "./main/main.component";
@@ -11,3 +12,22 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatDialogModule } from '@angular/material/dialog';
+import { NgModel } from "@angular/forms";
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        MainComponent,
+        AddProjectPopupComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        MatInputModule,
+        MatSelectModule,
+        MatButtonModule,
+        MatButtonToggleModule,
+        MatDialogModule
+    ]
+})
